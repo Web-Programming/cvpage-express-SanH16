008dce6666d7655508150356ac3c9e4fd31a54f7
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-10-09 02:19:13 +0700

    just add routing dir

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "San Tech Page" });
+  res.render("index", { title: "Adhitya Hasan" });
 });
 
 module.exports = router;
