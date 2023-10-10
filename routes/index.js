8618b130e8da16fa238e810db2c8e7ef140153cb
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-10-10 17:45:22 +0700

    update about

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var router = express.Router();
 
+const data = {
+  name: "Adhitya Hasan",
+  title: "About Me",
+  desc: "I can provide clean code and pixel perfect design. I also make the website more interactive with web animations. A responsive desgin makes your website accesible to all users, regardless of their device.",
+};
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Adhitya Hasan" });
+  res.render("index", { data });
 });
 
 module.exports = router;
